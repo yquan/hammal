aa625583c4d626dc5bc4524e7eabca2fc3b8440a
YQUAN243 <Yuan.Quan@marriott.com>
2024-08-22 16:27:48 +0800

    proxy nodesource too

--- src/handler.ts
+++ src/handler.ts
@@ -4,12 +4,14 @@ import { Backend } from './backend'
 const PROXY_HEADER_ALLOW_LIST: string[] = ["accept", "user-agent", "accept-encoding"]
 
 const ORG_NAME_BACKEND:{ [key: string]: string; } = {
+  "docker": "https://registry-1.docker.io",
   "gcr": "https://gcr.io",
   "k8sgcr": "https://k8s.gcr.io",
   "quay": "https://quay.io",
+  "nodesource": "https://deb.nodesource.com",
 }
 
-const DEFAULT_BACKEND_HOST: string = "https://registry-1.docker.io"
+const DEFAULT_BACKEND_ORG: string = "docker"
 
 export async function handleRequest(request: Request): Promise<Response> {
   return handleRegistryRequest(request)
@@ -26,28 +28,30 @@ function copyProxyHeaders(inputHeaders: Headers) : Headers {
 }
 
 function orgNameFromPath(pathname: string): string|null {
-  const splitedPath: string[] = pathname.split("/", 3)
-  if (splitedPath.length === 3 && splitedPath[0] === "" && splitedPath[1] === "v2") {
-    return splitedPath[2].toLowerCase()
+  const splitedPath: string[] = pathname.split("/", 2)
+  if (splitedPath.length === 2 && splitedPath[0] === "") {
+    return splitedPath[1].toLowerCase()
   }
-  return null
+  return DEFAULT_BACKEND_ORG
 }
 
 function hostByOrgName(orgName: string|null): string {
   if (orgName !== null && orgName in ORG_NAME_BACKEND) {
     return ORG_NAME_BACKEND[orgName]
   }
-  return DEFAULT_BACKEND_HOST
+  return ORG_NAME_BACKEND[DEFAULT_BACKEND_ORG]
 }
 
 function rewritePathByOrg(orgName: string|null, pathname: string): string {
-  if (orgName === null || !(orgName in ORG_NAME_BACKEND)) {
-    return pathname
-  }
+  // if (orgName === null || !(orgName in ORG_NAME_BACKEND)) {
+  //   console.log(`${pathname}`);
+  //   return pathname
+  // }
   const splitedPath: string[] = pathname.split("/")
   const cleanSplitedPath = splitedPath.filter(function(value: string, index: number) {
-    return value !== orgName || index !== 2;
+    return value !== orgName;
   })
+  console.log(`${cleanSplitedPath.join("/")}`);
   return cleanSplitedPath.join("/")
 }
 
@@ -59,5 +63,10 @@ async function handleRegistryRequest(request: Request): Promise<Response> {
   const tokenProvider = new TokenProvider()
   const backend = new Backend(host, tokenProvider)
   const headers = copyProxyHeaders(request.headers)
+
+  console.log(`reqURL: ${reqURL}`);
+  console.log(`orgName: ${orgName}`);
+  console.log(`pathname: ${pathname}`);
+  console.log(`host: ${host}`);
   return backend.proxy(pathname, {headers: request.headers})
 }
